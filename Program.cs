c6be46ceb3fecc3c2b2bc387b748877ecf561394
Carter S <speers.carter@gmail.com>
2024-05-24 12:40:34 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class Program
+{
+
+    public static void Main(String[] args)
+    {
+        double unitFactor = 2.54;
+        Console.WriteLine("enter inches");
+        var inches = Convert.ToDouble(Console.ReadLine());
+        double centimeters = inches * unitFactor;
+        Console.WriteLine($"{inches} inches is {centimeters} centimeters");
+    }
+}
---
+++ README.md
@@ -0,0 +1,16 @@
+## PSEUDOCODE
+```
+BEGIN
+	CLASS Program
+    CONST unitFactor = 2.54
+		Main METHOD
+			PRINT
+        - "enter inches"
+      VAR inches = [Unit Test Value]
+      VAR centimeter = inches * unitFactor
+      PRINT
+        - inches + " inches is " + centimeters + " centimeters" 
+		END Main
+	END Program
+END
+```
